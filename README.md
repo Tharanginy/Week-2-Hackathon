e3226bef9afda0fdf7e0877ece7aed554b597f9b
Tharanginy <tharanginym@outlook.com>
2025-07-11 11:31:24 +0100

    working dice roll

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Week-2-Hackathon
+
+# Roll a Dice Generator
+
+## Project description
+
+The user is asked in terminal if they want to roll the dice.
+When the user prompts 'y' for yes, a random number from a dice will be displayed and the user will be asked if they want to roll again. The user is prompted to respond with 'y' for yes or 'n' for no. 
+When the user prompts 'n' for no, no number is displayed.
+
+### Installation & usage
+Clone from Git repository or copy file.
+
+### Technologies
+- Javascript
+
+### Process
+
+### Screenshots/Images
+
+### Wins & Challenges (Code snippets)
+
+### Bugs
+
+### Future features
+- HTML
+- CSS
--- game.js
+++ game.js
@@ -1,1 +1,22 @@
+const readline = require('node:readline'); //allows terminal input
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(`Roll Dice?(Y/N)`, input => {
+    let input_case =input.toLowerCase(); //covert input lowercase
+    console.log('You have chosen:' + `${input_case}`) // informs user what they have picked
+
+    if (input_case == 'y'){ // if the input matches y for yes
+       result =  Math.ceil(Math.random() * 6) //choses a number between 0 and 1, rounds up for a number between 1 and 6
+        console.log(result); // shows the user the random outcome generated from dice
+    } else if(input_case == 'n'){
+        console.log('Have a nice day!')
+    }else { //when input is not yes or no
+        console.log('Error, try again!')
+    }
+
+  rl.close();
+});
